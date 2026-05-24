a3e06d00e584528e8ae643402b8cd3446233fda9
Salma Mohamed <salmaa001111@gmail.com>
2026-05-24 10:37:44 +0300

    Improve session state management in Login.py
    Refactor login session state initialization and user handling.

--- pages/Login.py
+++ pages/Login.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from db import init_db, add_user, check_user ,create_admin()
+from db import init_db, add_user, check_user,create_admin
 
 # =========================
 # PAGE CONFIG
@@ -7,18 +7,22 @@ from db import init_db, add_user, check_user ,create_admin()
 st.set_page_config(page_title="Login", page_icon="🔐")
 
 # =========================
-# INIT DB (once)
+# INIT DB
 # =========================
 init_db()
 create_admin()
 # =========================
-# SESSION STATE SAFETY
+# SESSION STATE SAFE INIT
 # =========================
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
+defaults = {
+    "logged_in": False,
+    "user": None,
+    "role": None
+}
 
-if "user" not in st.session_state:
-    st.session_state.user = None
+for k, v in defaults.items():
+    if k not in st.session_state:
+        st.session_state[k] = v
 
 # =========================
 # TITLE
@@ -42,18 +46,26 @@ with tab1:
             user = check_user(username, password)
 
             if user:
+                # user = (username, role)
+
                 st.session_state.logged_in = True
-                st.session_state.user = username
+                st.session_state.user = user[0]
+                st.session_state.role = user[1]
 
-                st.success("Login successful ✅")
+                st.success(f"Welcome {user[0]} 👋")
 
-                st.switch_page("pages/Dashboard.py")
+                # Redirect based on role
+                if user[1] == "admin":
+                    st.switch_page("pages/Admin.py")
+                else:
+                    st.switch_page("pages/Dashboard.py")
 
             else:
                 st.error("Invalid credentials ❌")
 
         else:
             st.warning("Please fill all fields")
+            
 
 # =========================
 # SIGN UP
