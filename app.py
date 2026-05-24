7b0f8869089f8d073b488f609e1386181674f692
Salma Mohamed <salmaa001111@gmail.com>
2026-05-24 09:26:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-st.set_page_config(
-    page_title="MindCare AI",
-    page_icon="🧠"
-)
-
-with st.sidebar:
-
-    option_menu(
-        "🧠 MindCare AI",
-        ["Login", "Dashboard", "Analytics", "Model Performance"],
-        icons=["lock", "cpu", "bar-chart", "graph-up"],
-        default_index=0
-    )
+st.set_page_config(page_title="MindCare AI 🧠")
 
 st.title("🧠 MindCare AI")
 
